cc32b249b6a3bc978a4fd268a793c0ddb238fb50
hagi4u <hagi4u@gmail.com>
2024-04-15 15:13:37 +0900

    feat: 글 상세 페이지 구현

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ import removeImports from 'next-remove-imports';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rzrozpdcyyxnwgvuzuny.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default removeImports(nextConfig);
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,14 +1,57 @@
+import { MarkdownViewer } from '@/components/Markdown';
+import { Post } from '@/types';
 import { createClient } from '@/utils/supabase/server';
+import { format } from 'date-fns';
 import { GetServerSideProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
 
-type PostProps = {
-  id: string;
-};
+type PostProps = Post;
 
-export default function Post({ id }: PostProps) {
+export default function PostPage({
+  id,
+  title,
+  category,
+  tags,
+  content,
+  created_at,
+  preview_image,
+}: PostProps) {
   return (
-    <div className="flex">
-      <h1>Post{id}</h1>
+    <div className="container mx-auto flex flex-col px-4 pb-40 pt-20">
+      <h1 className="mb-8 text-4xl font-bold">{title}</h1>
+      <div className="flex flex-row items-center gap-2">
+        <Link
+          href={`/categories/${category}`}
+          className="rounded-md bg-slate-800 px-2 py-1 text-sm text-white"
+        >
+          {category}
+        </Link>
+        {tags.map((tag) => (
+          <Link
+            href={`/tags/${tag}`}
+            key={tag}
+            className="rounded-md bg-slate-800 px-2 py-1 text-sm text-white"
+          >
+            {tag}
+          </Link>
+        ))}
+        <div className="text-sm text-gray-400">
+          {format(new Date(created_at), 'yyyy년 M월 d일 HH:mm')}
+        </div>
+      </div>
+
+      {preview_image && (
+        <Image
+          src={preview_image}
+          width={0}
+          height={0}
+          sizes="100vw"
+          alt={title}
+          className="mt-8 h-auto w-full"
+        />
+      )}
+      <MarkdownViewer source={content} className="mt-8 min-w-full " />
     </div>
   );
 }
@@ -19,12 +62,25 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const { id } = query;
   const supabase = createClient(req.cookies);
-  const response = await supabase.from('Post').select('*').eq('id', Number(id));
 
-  console.log('response: ', response);
+  const { data } = await supabase.from('Post').select('*').eq('id', Number(id));
+
+  if (!data || !data[0]) {
+    return { notFound: true };
+  }
+
+  const { title, category, created_at, tags, content, preview_image } = data[0];
+
+  console.log('response: ', data);
   return {
     props: {
       id,
+      title,
+      category,
+      created_at,
+      tags: JSON.parse(tags) as string[],
+      content,
+      preview_image,
     },
   };
 };
