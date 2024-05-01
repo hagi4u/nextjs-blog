54e1e890035733605d50e3e226289ab6e24cb9a5
hagi4u <hagi4u@gmail.com>
2024-05-01 11:01:39 +0900

    feat: 메인페이지 infiniteQuery 적용해서 더보기 기능 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,58 @@
 import PostCard from '@/components/PostCard';
 import { createClient } from '@/utils/supabase/client';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 const supabase = createClient();
 
 export default function Home() {
-  const { data: posts } = useQuery({
+  const { ref, inView } = useInView();
+  const {
+    data: postPages,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
     queryKey: ['posts'],
-    queryFn: async () => {
+    queryFn: async ({ pageParam }) => {
       const { data } = await supabase
         .from('Post')
         .select('*')
-        .order('created_at', { ascending: false });
+        .order('created_at', { ascending: false })
+        .range(pageParam, pageParam + 3);
 
       if (!data) {
-        return [];
+        return {
+          posts: [],
+          nextPage: null,
+        };
       }
-      return data;
+      return {
+        posts: data,
+        nextPage: data.length === 4 ? pageParam + 4 : null,
+      };
     },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
   });
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
+  if (!postPages) {
+    return null;
+  }
   return (
-    <div className="container mx-auto grid grid-cols-2 gap-x-4 gap-y-6 px-4 pb-24 pt-20 lg:gap-x-7 lg:gap-y-12">
-      {posts?.map((post) => <PostCard key={post.id} {...post} />)}
+    <div className="flex flex-col">
+      <div className="container mx-auto grid grid-cols-2 gap-x-4 gap-y-6 px-4 pb-24 pt-20 lg:gap-x-7 lg:gap-y-12">
+        {postPages?.pages
+          .flatMap((page) => page.posts)
+          .map((post) => <PostCard key={post.id} {...post} />)}
+      </div>
+      <div ref={ref} />
     </div>
   );
 }
