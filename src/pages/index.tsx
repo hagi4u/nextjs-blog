a612d77034563119c32ad5268317fca4b41ccdc9
hagi4u <hagi4u@gmail.com>
2024-05-01 10:25:46 +0900

    feat: PostCard 컴포넌트 추가 후 메인페이지에서 렌더링되게 개선

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-import { Inter } from 'next/font/google';
+import PostCard from '@/components/PostCard';
+import { createClient } from '@/utils/supabase/client';
+import { useQuery } from '@tanstack/react-query';
 
-const inter = Inter({ subsets: ['latin'] });
+const supabase = createClient();
 
 export default function Home() {
+  const { data: posts } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      const { data } = await supabase
+        .from('Post')
+        .select('*')
+        .order('created_at', { ascending: false });
+
+      if (!data) {
+        return [];
+      }
+      return data;
+    },
+  });
   return (
-    <main className="h-[200px]">
-      <h1>Hello world</h1>
-    </main>
+    <div className="container mx-auto grid grid-cols-2 gap-x-4 gap-y-6 px-4 pb-24 pt-20 lg:gap-x-7 lg:gap-y-12">
+      {posts?.map((post) => <PostCard key={post.id} {...post} />)}
+    </div>
   );
 }
