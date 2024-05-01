85f6a7a694b48b644777ef87f6e49fc71f5e8427
hagi4u <hagi4u@gmail.com>
2024-05-02 08:05:30 +0900

    feat: 카테고리, 태그 목록 페이지 SSG 로 전환

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,26 @@
 import PostList from '@/components/PostList';
+import { Post } from '@/types';
+import { createClient } from '@/utils/supabase/server';
+import { GetStaticProps } from 'next';
+
+export const getStaticProps = (async () => {
+  const supabase = createClient({});
+  const { data } = await supabase.from('Post').select('*');
+
+  return {
+    props: {
+      posts:
+        data?.map((post) => {
+          return {
+            ...post,
+            tags: JSON.parse(post.tags),
+          };
+        }) ?? [],
+    },
+  };
+}) satisfies GetStaticProps<{
+  posts: Post[];
+}>;
 
 export default function Home() {
   return <PostList />;
